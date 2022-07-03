6a891d1edae69cb2fa4494fa86e08d81537ce429
coderFeedForwardAlg <gaspersscottma@gmail.com>
2022-07-03 06:05:55 -0400

    new commit

---
+++ src/components/landing.js
@@ -0,0 +1,15 @@
+const Landing = () => {
+    return ( 
+        <div className="landing-page">
+            <h1>
+                Welcome to Productivity Pangolin. 
+            </h1>
+            <br/>
+            <h3>
+                This is a great place to make sure you are working hard and taking breaks! 
+            </h3>
+        </div>
+     );
+}
+ 
+export default Landing;
